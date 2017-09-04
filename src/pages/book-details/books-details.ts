6841940865c27a396a6a78b0d2cdf37b30389ad4
AlinaPulya <alvasamoilova@gmail.com>
2017-09-04 16:08:05 +0300

    Add method to close book details modal page

--- src/pages/book-details/books-details.ts
+++ src/pages/book-details/books-details.ts
@@ -1,5 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {NavController} from 'ionic-angular';
+
+import { ViewController } from 'ionic-angular';
+import { Book } from '../../models';
 
 @Component({
   selector: 'books-details-page',
@@ -9,7 +12,11 @@ import {NavController} from 'ionic-angular';
  *@class Represents Book Details Page
  */
 export class BooksDetailsPage {
+  @Input() public book: Book;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public viewCtrl: ViewController) {
   }
+  closeModal() {
+    this.viewCtrl.dismiss();
+}
 }
