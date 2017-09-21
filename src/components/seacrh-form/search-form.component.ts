18c481adf848f34e19b340af12db502f1d5e403f
AlinaPulya <alvasamoilova@gmail.com>
2017-09-21 20:42:33 +0300

    Change core of search.html

--- src/components/seacrh-form/search-form.component.ts
+++ src/components/seacrh-form/search-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Book } from '../../models';
+import {BooksService} from '../../services';
 
 @Component({
   selector: 'search-form',
@@ -14,9 +15,13 @@ export class SearchFormComponent {
   public searchQuery: string;
   public bookItems: Array<Book>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private booksService: BooksService, public navCtrl: NavController) {
+    // let searchValue = document.getElementById('search') as HTMLInputElement;
+    // this.searchQuery = searchValue.value;
+    booksService.get(this.searchQuery).subscribe(books => this.bookItems = books);
+    console.log(this.searchQuery);
   }
-  setSearchParams(event){
+  setSearchParams(event: any){
   }
 
 }
