5c8f32f5c94266ce1d9e6cbe384d6ccda7019121
Lymanska Daria <daria.lymanska@gmail.com>
2017-09-28 17:41:12 +0300

    Add avarage raiting element

--- src/app/components/books/book-item.component.ts
+++ src/app/components/books/book-item.component.ts
@@ -15,12 +15,30 @@ import { BooksDetailsPage } from '../../pages/book-details/books-details';
 export class BookItemComponent {
   @Input() public book: Book;
 
+  private stars: string[] = [];
+
   constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+
+  }
+
+  ngOnInit() {
+    if (!this.book || !this.book.rating || !this.book.rating.average) return;
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= this.book.rating.average) {
+          this.stars.push('md-star')
+      } else {
+        if (this.book.rating.average > (i-1)) {
+          this.stars.push('md-star-half')
+        } else {
+          this.stars.push('md-star-outline')
+        }
+      }
+    }
   }
 
   openModal() {
     let myModal = this.modalCtrl.create(BooksDetailsPage, {book: this.book});
     myModal.present();
   }
-
 }
