f74341ade6d2039fb945e1883894ab6784ec498f
AlinaPulya <alvasamoilova@gmail.com>
2017-10-23 11:05:17 +0300

    Add oauth-providers.list.page and oauth-profile.page

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -3,3 +3,4 @@ export * from './home';
 export * from './favorites';
 export * from './book-details';
 export * from './oauth/list/oauth-providers.list.page';
+export * from './oauth/profile/oauth-profile.page'
