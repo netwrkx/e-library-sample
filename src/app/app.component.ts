9f740f2d9f5646022b73c79e8dbed75a3d2e60b6
Ljubow <lubovobrusnik@gmail.com>
2017-11-07 13:23:39 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
-// import {TabsPage} from './pages/tabs/tabs';
+import {TabsPage} from './pages/tabs/tabs';
 import {HomePage} from './pages';
 
 @Component({
@@ -11,7 +11,7 @@ import {HomePage} from './pages';
   styleUrls: ['/app.scss']
 })
 export class ELibraryApp {
-  rootPage: any = HomePage;
+  rootPage: any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
